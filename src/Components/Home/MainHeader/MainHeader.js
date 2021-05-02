19e70768babef82e25c09e99be8b3b6684a9e57f
Shuvo Goswami <avitalukder20158@gmail.com>
2021-05-02 21:33:22 +0600

    profile added

--- src/Components/Home/MainHeader/MainHeader.js
+++ src/Components/Home/MainHeader/MainHeader.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import Profile from '../../Images/Profile.jpg';
 
 const MainHeader = () => {
     return (
-        <div className="text-white d-flex flex-column align-items-center justify-content-center" style={{ height: "735px" }}>
-            <div className="">
-                <p style={{ fontSize: "60px" }}>Hi, I am Avishek Talukdar</p>
+        <div className="row d-flex align-items-center w-100" style={{ height: "730px" }}>
+            <div className="col-md-5 offset-md-1 col-sm-6 col-12">
+                <img src={Profile} alt="" className="img-fluid" />
             </div>
-            <div className="">
-                <p style={{ fontSize: "30px" }}>Welcome to My Website</p>
+            <div className="col-md-6 col-sm-6 col-12">
+                <div className="text-white text-center">
+                    <p style={{ fontSize: "60px" }}>Hi, I am Avishek Talukdar</p>
+                    <p style={{ fontSize: "30px" }}>Welcome to My Website</p>
+                    <Button variant="outline-secondary" className="text-white">My Resume</Button>
+                </div>
             </div>
-            <Button variant="outline-secondary" className="text-white">My Resume</Button>
         </div>
     );
 };
