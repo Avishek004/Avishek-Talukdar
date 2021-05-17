2b6f4bb24ae1fb6becec1b86558ae0aef935f019
Shuvo Goswami <avitalukder20158@gmail.com>
2021-05-18 01:19:50 +0600

    about fixed

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import Profile from '../Images/Profile.png';
+import { Container } from 'react-bootstrap';
+import Profile from '../Images/Profile.jpg';
 
 const AboutMe = () => {
     return (
-        <div className="p-5" id="aboutMe">
-            <h1 className="text-uppercase text-center pb-3">About Me</h1>
-            <div className="row">
-                <div className="col-md-5 offset-md-1 col-sm-6 col-12 p-3">
-                    <img src={Profile} alt="" className="img-fluid" />
+        <section className="p-5">
+            <Container>
+                <div id="aboutMe">
+                    <h1 className="text-uppercase text-center pb-3">About Me</h1>
+                    <div className="row">
+                        <div className="col-md-6 col-sm-6 col-12 p-3">
+                            <img src={Profile} alt="" className="img-fluid" />
+                        </div>
+                        <div className="col-md-6 col-sm-6 col-12 p-3">
+                            <h4>Hello! I'm Avishek Talukdar</h4>
+                            <p className="text-dark">I am a self-motivated Web Developer who wants to learn anything as he wants to.</p>
+                            <p className="text-dark">I want to live my life differently & I wish to see the world in new, better & helpful ways than it is now. I want to learn new skills for turning myself into a better person than I am. I always take my work seriously & I have a driven mind to make my job done. I take responsibility for what I do.</p>
+                            <p className="text-dark">I love travelling a lot. Nature, especially Mountain & Sea, attracts me very much.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-6 col-sm-6 col-12 p-3">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis consequatur accusamus obcaecati. Harum maiores quidem, perferendis eveniet maxime, similique culpa dolorum ea aliquid sunt repellendus ratione quia accusantium cupiditate autem voluptatem fugiat error distinctio totam quisquam ab consequatur natus, consequuntur reiciendis! Quo, inventore consequatur accusantium quidem ipsa eveniet voluptate tempore.</p>
-                </div>
-            </div>
-        </div>
+            </Container>
+        </section>
     );
 };
 
--- src/Components/Home/HeaderTop/HeaderTop.js
+++ src/Components/Home/HeaderTop/HeaderTop.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import whiteLogo from '../../Images/logo-white.png';
+import './HeaderTop.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const HeaderTop = () => {
     return (
-        <div className="m-0 p-0 w-100">
-            <Navbar collapseOnSelect expand="lg">
-                <Navbar.Brand href="#"><img src={whiteLogo} alt="" className="w-25 h-25 ml-5" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto mr-5">
-                        <Nav.Link href="#projects" className="text-white mr-3">Projects</Nav.Link>
-                        <Nav.Link href="#blogs" className="text-white mr-3">Blogs</Nav.Link>
-                        <Nav.Link href="#aboutMe" className="text-white text-uppercase mr-3">About Me</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+        <div className="m-0 p-0">
+            <Container>
+                <Navbar collapseOnSelect expand="lg" className="custom-nav" variant="dark">
+                    <Navbar.Brand href="#"><span className="text-white">Avishek</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#projects" className="text-white">Projects</Nav.Link>
+                            <Nav.Link href="#blogs" className="text-white">Blogs</Nav.Link>
+                            <Nav.Link href="#aboutMe" className="text-white">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
         </div>
     );
 };
